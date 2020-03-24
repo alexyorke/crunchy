da38993307c05cf54d31a3e0318df0fd93302358
Alex Yorke <7844441+alexyorke@users.noreply.github.com>
2020-03-23 22:16:12 -0400

    Create crunchy.js

---
+++ crunchy.js
@@ -0,0 +1,26 @@
+let timer;
+
+const z = Zone.current.fork({
+  name: "z",
+  onInvokeTask(delegate, currentZone, targetZone, task, ...args) {
+    function sleep(sleepDuration) {
+      var now = new Date().getTime();
+      while (new Date().getTime() < now + sleepDuration) {
+        /* do nothing */
+      }
+    }
+    sleep(Math.round(Math.random() * 1000));
+    return delegate.invokeTask(targetZone, task, ...args);
+  }
+});
+
+function b() {
+  timer = Date.now();
+  console.log("Synchronous task 1");
+  setTimeout(function() {
+    console.log("Async task 1");
+  }, 0);
+  console.log("Synchronous task 2");
+}
+
+z.run(b);
